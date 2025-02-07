880aa3f368dc3d8f50103fe2a660b1f828857b62
David <david12ea@gmail.com>
2025-02-06 19:28:50 -0500

    Agrega array y empieza a hacer la función agregar lista

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo(){
+    let amigoUsuario = document.getElementById('amigo').value;
+    listaAmigos.push[amigoUsuario];
+    for (let i = 0; i < listaAmigos.length;i++){
+        textoElemento('ul',);
+    }
+    return;
+    
+    
+}
+
+function textoElemento(elemento, texto){
+    let elementoHtml = document.querySelector(elemento);
+    elementoHtml.innerHTML = texto;
+    return;
+}
+
