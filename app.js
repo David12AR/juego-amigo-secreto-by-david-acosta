094e7453fb5b8a1f0012f83778815f2eae9f5ece
David <david12ea@gmail.com>
2025-02-08 12:41:43 -0500

    Se agrega la función agregar amigos y se está desarrollando la de actualizar amigos

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let listaAmigosArray = [];// Se crea el arreglo vacio.
 
+//Se crea la función agregar amigo para que sea agregado al arreglo.
 function agregarAmigo(){
-    let amigoUsuario = document.getElementById('amigo').value;
-    listaAmigos.push[amigoUsuario];
-    for (let i = 0; i < listaAmigos.length;i++){
-        textoElemento('ul',);
+    
+    let amigoUsuario = document.getElementById('amigo').value;//Asignar
+    if (amigoUsuario == ""){
+        alert("Por favor inserta un nombre")
+    }else{
+        listaAmigosArray.push(amigoUsuario);
     }
+    limpiaTextBox();
+    recorrerArray();
     return;
     
     
 }
 
-function textoElemento(elemento, texto){
-    let elementoHtml = document.querySelector(elemento);
-    elementoHtml.innerHTML = texto;
-    return;
+
+function recorrerArray(){
+    let listaHtml = document.getElementById('listaAmigos');
+    listaHtml.innerHTML="";
+    listaAmigosArray.forEach(function(amigoUsuario){
+        console.log(amigoUsuario)
+        let agregarAmigoLista =document.createElement('li');
+        agregarAmigoLista.textContent = amigoUsuario;
+        listaHtml.appendChild(agregarAmigoLista);
+
+    });
+
+}
+
+
+
+function limpiaTextBox(){
+    document.getElementById('amigo').value = "";
 }
 
